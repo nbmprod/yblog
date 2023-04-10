611a0d244f10ebd02f23acb561efb6a31cbe5a9e
Nikita Bychkov <nikitabychkov96@gmail.com>
2023-04-10 11:56:11 +0600

    init + Navbar

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<script>
+import Navbar from './components/Navbar.vue';
+
+export default{
+  name: 'App',
+  components: { Navbar },
+
+  // data() {
+  //   return {
+  //     blogPage: false,
+  //   }
+  // }
+
+}
+
+</script>
+
+<template>
+  <div class="page">
+    <Navbar :blogPage=true />
+  </div>
+  <footer class="footer">
+  </footer>
+</template>
+
+<style>
+.page {
+  display: flex;
+  flex-direction: column;
+  flex: 1 0 auto;
+}
+
+.container {
+  max-width: 1110px;
+  margin: 0 auto;
+  box-sizing: border-box;
+}
+
+.footer {
+  background-color: var(--grey-2);
+  margin-top: 3em;
+  flex-shrink: 0;
+}
+</style>
---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import sass from 'sass'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  css: {
+    preprocessorOptions: {
+      sass: {
+        implementation: sass,
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
